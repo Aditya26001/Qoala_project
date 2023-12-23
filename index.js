def9981eb2ee5d3b0e38cdb5b24ab6f5809b0ff4
Aditya <adirkmv2019@gmail.com>
2023-12-23 19:41:45 +0530

    Database connected

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./backend/src/db/connection.js";
+
+dotenv.config()
+
 const app  = express()
 
 const port = process.env.PORT||3000
 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||3000,()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection Error",err);
 })
